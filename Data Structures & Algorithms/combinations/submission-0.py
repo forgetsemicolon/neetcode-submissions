98eacc8da434cbc9bb335f1f430bcb5d8a17381f
forgetsemicolon <pagarwal2610@gmail.com>
2026-06-18 10:34:13 -0700

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if len(subset) >= k:
+                res.append(subset.copy())
+                return
+
+            if i <= n:
+                subset.append(i)
+
+            else:
+                return
+
+
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
+
+        dfs(1)
+        return res
+
+
